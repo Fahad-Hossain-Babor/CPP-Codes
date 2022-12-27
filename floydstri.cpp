44a55ef061eb72e665801c20dff1d18f82da0aaf
Fahad Hossain Babor <111192242+Fahad-Hossain-Babor@users.noreply.github.com>
2022-12-27 13:46:58 +0600

    Create floydstri.cpp

---
+++ floydstri.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{   int count;
+    count =1;
+    int n;
+    cout << "Enter your number : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<count<< " ";
+            count++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+OUTPUT :
+
+Enter your number : 5
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
