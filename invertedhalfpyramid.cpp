e19ebf9ce460e5ae2753ce9da628a69948e3080f
Fahad Hossain Babor <111192242+Fahad-Hossain-Babor@users.noreply.github.com>
2022-12-27 13:44:48 +0600

    Create invertedhalfpyramid.cpp

---
+++ invertedhalfpyramid.cpp
@@ -0,0 +1,29 @@
+ // printing a inverted half pyramid
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a num : ";
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+output:
+
+Enter a num : 6
+ *  *  *  *  *  * 
+ *  *  *  *  * 
+ *  *  *  * 
+ *  *  * 
+ *  * 
+ * 
